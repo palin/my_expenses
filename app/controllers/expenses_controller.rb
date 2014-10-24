7e6a53e1f2508de39e5156031b69137316cf31eb
Maciek Paliński <maciekpalinski@gmail.com>
2014-10-24 20:45:41 +0100

    a bit of cleanup

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,5 +1,13 @@
 class ExpensesController < ApplicationController
-  def index
 
+  before_filter :require_logged_user, only: [:index]
+
+  def index
+  end
+
+  private
+
+  def require_logged_user
+    redirect_to root_path, alert: "You have to be logged in to see this page!" and return unless current_user.present?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
+  get 'auth/facebook', as: 'auth_provider'
+  get 'auth/failure', to: 'sessions#destroy'
+  get 'auth/facebook/callback', to: 'sessions#create'
   delete 'sessions/destroy', to: 'sessions#destroy'
 
   resources :expenses, only: [:index]
 
   root to: 'home#index'
-  get 'auth/facebook', as: 'auth_provider'
-  get 'auth/facebook/callback', to: 'sessions#create'
-
 end
