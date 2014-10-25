56ce24dce0c2449d9932e69685be861ab715da26
Maciek Paliński <maciekpalinski@gmail.com>
2014-10-25 20:35:13 +0100

    create new expense form

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -9,7 +9,17 @@ class ExpensesController < ApplicationController
     @expense = Expense.new
   end
 
+  def create
+    current_user.expenses.create(expense_params)
+
+    redirect_to new_expense_path
+  end
+
   private
+
+  def expense_params
+    params.require(:expense).permit(:name, :payed_at, :user_id)
+  end
 
   def require_logged_user
     redirect_to root_path, alert: "You have to be logged in to see this page!" and return unless current_user.present?
