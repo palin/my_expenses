41118e00f1c745552992e38fc9f7c5b37ea54f5e
Maciek Paliński <maciekpalinski@gmail.com>
2014-11-27 01:51:46 +0000

    new route for dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  root to: 'home#index'
+
   get 'auth/facebook', as: 'auth_provider'
   get 'auth/failure', to: 'sessions#destroy'
   get 'auth/facebook/callback', to: 'sessions#create'
   delete 'sessions/destroy', to: 'sessions#destroy'
 
-  resources :expenses, only: [:index, :new, :create, :edit, :update, :destroy]
+  namespace :dashboard do
+    root to: redirect('dashboard/expenses/new')
 
-  root to: 'home#index'
+    resources :expenses, only: [:index, :new, :create, :edit, :update, :destroy]
+    resource :profile, only: [:show, :edit, :update, :destroy]
+    resource :settings, only: [:edit, :update]
+  end
 end
