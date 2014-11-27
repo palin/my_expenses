90c8058da13d451323ef23c181ecaec832aa9a92
Maciek Paliński <maciekpalinski@gmail.com>
2014-11-27 02:20:31 +0000

    use helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def translate(key)
+    module_part = controller.class.name.split("::").first.downcase
+    controller_part = controller_name
+
+    I18n.t("#{module_part}.#{controller_part}.#{action_name}.#{key}")
+  end
 end
