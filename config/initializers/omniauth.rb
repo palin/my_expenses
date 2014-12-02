5892382f238022ef5d04f0f8a6a2c8eb4dce807a
Maciek Paliński <maciekpalinski@gmail.com>
2014-12-02 12:04:37 +0000

    use yaml or env variable

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, FACEBOOK_CONFIG['app_id'], FACEBOOK_CONFIG['secret'], {:scope => 'email,user_about_me'}
+  app_id = FACEBOOK_CONFIG['app_id'] || ENV['fb_app_id']
+  secret = FACEBOOK_CONFIG['secret'] || ENV['fb_secret']
+  provider :facebook, app_id, secret, {:scope => 'email,user_about_me'}
 end
